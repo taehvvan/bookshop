2672e5b7feccb2c5071144c983f1d56107f41821
taehvvan <th00514@naver.com>
2025-09-22 13:39:24 +0900

    6

--- src/main/java/main/HomeController.java
+++ src/main/java/main/HomeController.java
@@ -6,10 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    // http://localhost:8888/ 로 접속하면 이 메서드가 실행됩니다.
     @GetMapping("/")
-    public String redirectToMain() {
-        // "redirect:/main"은 사용자를 /main 경로로 다시 보내라는 뜻입니다.
-        return "redirect:/main";
+    public String testPage() {
+        return "test";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>🎉 축하합니다! 페이지가 성공적으로 로드되었습니다! 🎉</h1>
+    <p>이 페이지가 보인다면, Spring MVC 웹 설정은 완벽하게 동작하는 것입니다.</p>
+    <p>이제 다음 단계는 데이터베이스 연결 설정을 다시 확인하는 것입니다.</p>
+</body>
+</html>
